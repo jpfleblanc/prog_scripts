0dfce395b7a370e87e305764766bd5dca5a1b34a
James LeBlanc <jpfleblanc@gmail.com>
2016-04-28 11:58:53 -0400

    setpm3d script

---
+++ set_pm3d.py
@@ -0,0 +1,27 @@
+import math,zlib,numpy,sys
+import os
+import numpy
+inputfile = sys.argv[1]
+outputfile = sys.argv[2]
+
+f = open(outputfile,'w')
+#f.write('hi there\n') # python will convert \n to os.linesep
+#f.close() 
+
+qx, qy, w, real = numpy.loadtxt(inputfile, unpack=True)
+
+
+qlast=qx[0]
+for i in range(0, len(qx)):
+  f.write(str(qx[i])+" "+str(qy[i])+" "+str(w[i])+" "+str(real[i])+"\n")
+  if qx[i] != qlast:
+    qlast=qx[i]
+    f.write("\n")
+
+
+
+
+
+
+
+
